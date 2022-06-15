8cc0b1c8da020703794961525a4658837437e384
Anuar Nurshinov <anuarnurshinov@gmaul.com>
2022-06-15 07:48:55 +0300

    Урок 11

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+'use strict'
+const btn = document.querySelector("#e_btn");
+const input = document.querySelector("#text")
+const square = document.querySelector("#square")
+const circle = document.querySelector("#circle")
+const inputRange = document.querySelector("#range")
+
+const changeColor = function () {
+    console.log(input.value)
+    square.style.backgroundColor = `${input.value}`
+    input.value = ''
+    btn.style.display = 'none'
+}
+
+const changeSize = function () {
+    circle.style.width = `${inputRange.value}%`
+    circle.style.height = `${inputRange.value}%`
+}
 
 
 
 
 
+
+
+
+btn.addEventListener('click', changeColor);
+inputRange.addEventListener('input', changeSize);
